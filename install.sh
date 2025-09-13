7e59ed0aaacadab933ca7accfe5541b59637c946
ELMatero6 <150314379+ELMatero6@users.noreply.github.com>
2025-09-12 22:51:57 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "[CarOS] Starting installation..."
-echo "[CarOS] This is a test script, it doesn't actually install anything yet. But it will later"
-echo "[CarOS] Installation complete!"
-echo "Hello world"
